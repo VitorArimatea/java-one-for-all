5b4b6f996b347f22c28df6178c769e8d661348e5
VitorArimatea <vitorarimatea@hotmail.com>
2025-02-07 19:39:29 -0300

    new: DayOfTheWeek 1 per 1

---
+++ src/academy/arimatea/maratonajava/introducao/Aula05ConditionalStatements05.java
@@ -0,0 +1,50 @@
+package academy.arimatea.maratonajava.introducao;
+
+public class Aula05ConditionalStatements05 {
+    public static void main(String[] args) {
+        // Print the day of the week, considering 1 as Sunday
+        byte dayOfTheWeek = 7;
+
+        switch (dayOfTheWeek) {
+            case 1:
+                System.out.println("Sunday");
+                break;
+            case 2:
+                System.out.println("Monday");
+                break;
+            case 3:
+                System.out.println("Tuesday");
+                break;
+            case 4:
+                System.out.println("Wednesday");
+                break;
+            case 5:
+                System.out.println("Thursday");
+                break;
+            case 6:
+                System.out.println("Friday");
+                break;
+            case 7:
+                System.out.println("Saturday");
+                break;
+            default:
+                System.out.println("Invalid Option!");
+                break;
+        }
+
+        char sex = 'F';
+
+        switch (sex) {
+            case 'M':
+                System.out.println("Man");
+                break;
+            case 'F':
+                System.out.println("Woman");
+                break;
+            default:
+                System.out.println("Invalid option!");
+                break;
+        }
+
+    }
+}
