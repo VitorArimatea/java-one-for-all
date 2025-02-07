07585ab6deb6d502499d97a2c7890432a608e280
VitorArimatea <vitorarimatea@hotmail.com>
2025-02-07 19:41:29 -0300

    new: Basic Repetition Structures

---
+++ src/academy/arimatea/maratonajava/introducao/Aula06RepetitionStructures01.java
@@ -0,0 +1,21 @@
+package academy.arimatea.maratonajava.introducao;
+
+public class Aula06RepetitionStructures01 {
+    public static void main(String[] args) {
+        // while, do while, for
+
+        int count = -1;
+
+        while (count <= 20) {
+            System.out.println("While: " + count++);
+        }
+
+        do {
+            System.out.println("Do while: " + count++);
+        } while (count <= 30);
+
+        for (int i = 0; i <= 10; i++) {
+            System.out.println("For: " + i);
+        }
+    }
+}
