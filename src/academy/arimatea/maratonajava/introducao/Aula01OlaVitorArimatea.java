ff61b40df69fc598cdcfc4ccece628b8741390b6
VitorArimatea <vitorarimatea@hotmail.com>
2025-02-07 10:23:08 -0300

    First commit

---
+++ src/academy/arimatea/maratonajava/introducao/Aula01OlaVitorArimatea.java
@@ -0,0 +1,19 @@
+// Isso diz onde a classe está localizada
+package academy.arimatea.maratonajava.introducao;
+
+/**
+ * Isso é um comentário <strong>javadoc</strong>
+ */
+
+public class Aula01OlaVitorArimatea {
+    // Isso é um comentário
+     /*
+        Isso é outro comentário
+        de múltiplas
+        linhas
+     */
+    public static void main (String[] args) {
+        System.out.println("Olá! Eu sou o Vítor.");
+        System.out.print("Sou um Desenvolvedor de software");
+    }
+}
