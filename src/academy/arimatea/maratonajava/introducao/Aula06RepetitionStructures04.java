4e7af3f44a64ad5037d4be65a026717db5616bb2
VitorArimatea <vitorarimatea@hotmail.com>
2025-02-08 12:54:42 -0300

    new: Break Exercise

---
+++ src/academy/arimatea/maratonajava/introducao/Aula06RepetitionStructures04.java
@@ -0,0 +1,24 @@
+package academy.arimatea.maratonajava.introducao;
+
+public class Aula06RepetitionStructures04 {
+    public static void main(String[] args) {
+        // Given the price of a car, determine in how many installments it can be paid
+        // Condition installmentPrice >= 1000
+
+        double carValue = 20000;
+
+        for (int installment = 1; installment <= carValue; installment++) {
+            double installmentPrice = carValue / installment;
+
+            /*if (installmentPrice < 1000) {
+                break;
+            }*/
+
+            if (installmentPrice >= 1000) {
+                System.out.println("Installment: " + installment + " R$ " + installmentPrice);
+            } else {
+                break;
+            }
+        }
+    }
+}
