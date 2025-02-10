eb7b425cbac6b6f2c7eb5687730e82c86bedf224
VitorArimatea <vitorarimatea@hotmail.com>
2025-02-10 09:37:01 -0300

    new: Arrays Introduction

---
+++ src/academy/arimatea/maratonajava/introducao/Aula07Arrays01.java
@@ -0,0 +1,11 @@
+package academy.arimatea.maratonajava.introducao;
+
+public class Aula07Arrays01 {
+    public static void main(String[] args) {
+        String[] names = new String[]{"Vítor", "Marina", "Rebeca", "Mariana"};
+        names[0] = "Vítor Arimatéa";
+        int[] numbers = new int[]{1, 5, 6, 9};
+        String[] programmingLanguages = new String[3];
+        System.out.println(names[0]);
+    }
+}
