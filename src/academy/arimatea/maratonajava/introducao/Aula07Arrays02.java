fb0a56e78121b7eab991972fec0287b042e0d2ec
VitorArimatea <vitorarimatea@hotmail.com>
2025-02-10 09:47:07 -0300

    new: Initial Values in Arrays

---
+++ src/academy/arimatea/maratonajava/introducao/Aula07Arrays02.java
@@ -0,0 +1,15 @@
+package academy.arimatea.maratonajava.introducao;
+
+public class Aula07Arrays02 {
+    public static void main(String[] args) {
+        // byte, short, int, long, float & double 0
+        // char '\u0000'
+        // boolean false
+        // String null
+
+        int[] moneys = new int[3];
+        System.out.println(moneys[0]);
+        System.out.println(moneys[1]);
+        System.out.println(moneys[2]);
+    }
+}
