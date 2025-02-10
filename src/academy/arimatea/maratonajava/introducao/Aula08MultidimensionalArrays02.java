fdd373256b5e9d91864688756af0e8ad2a6e3cd9
VitorArimatea <vitorarimatea@hotmail.com>
2025-02-10 11:47:28 -0300

    new: Multidimensional Arrays Example

---
+++ src/academy/arimatea/maratonajava/introducao/Aula08MultidimensionalArrays02.java
@@ -0,0 +1,24 @@
+package academy.arimatea.maratonajava.introducao;
+
+public class Aula08MultidimensionalArrays02 {
+    public static void main(String[] args) {
+
+        int[] array = {1, 2, 3, 4};
+
+        int[][] arrayInt = new int[4][0];
+
+        arrayInt[0] = new int[2];
+        arrayInt[1] = new int[]{1, 2, 3};
+        arrayInt[2] = array;
+        arrayInt[3] = new int[5];
+
+        int[][] arrayInt2 = {{0, 0}, {1, 2, 3}, {1, 2, 3, 4, 5, 6}};
+
+        for (int[] baseArray : arrayInt2) {
+            System.out.println("\n--------");
+            for (int num : baseArray) {
+                System.out.print(num + "  ");
+            }
+        }
+    }
+}
