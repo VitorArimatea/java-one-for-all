94fd1c641ae12f0daceb6462fca447be68922d1a
VitorArimatea <vitorarimatea@hotmail.com>
2025-02-12 08:22:49 -0300

    Update: Calculator Example

---
+++ src/academy/arimatea/maratonajava/javacore/introductionmethods/domain/Calculator.java
@@ -0,0 +1,42 @@
+package academy.arimatea.maratonajava.javacore.introductionmethods.domain;
+
+public class Calculator {
+
+    public void sumTwoNumbers() {
+        System.out.println("Sum result: " + 15 + 10);
+    }
+
+    public void subTwoNumbers() {
+        int sub = 15 - 10;
+
+        System.out.println("Sub result: " + sub);
+    }
+
+    public void multTwoNumbers(int n1, float n2) {
+        System.out.println("Mult result: " + n1 * n2);
+    }
+
+    public double divTwoNumbers(double n1, double n2) {
+        if (n2 == 0) {
+            return 0;
+        }
+
+        return n1 / n2;
+    }
+
+    public void printDivTwoNumbers(int n1, int n2) {
+        if (n2 != 0) {
+            int div = n1 / n2;
+            System.out.println(div);
+            return;
+        }
+        System.out.println("Division by 0 does not exist");
+    }
+
+    public void changeTwoNumbers(int num1, int num2) {
+        num1 = 70;
+        num2 = 40;
+
+        System.out.println("Inside changeTwoNumbers: " + num1 + ", " + num2);
+    }
+}
---
+++ src/academy/arimatea/maratonajava/javacore/introductionmethods/test/CalculatorTest02.java
@@ -0,0 +1,15 @@
+package academy.arimatea.maratonajava.javacore.introductionmethods.test;
+
+import academy.arimatea.maratonajava.javacore.introductionmethods.domain.Calculator;
+
+public class CalculatorTest02 extends Calculator {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        int num1 = 1;
+        int num2 = 2;
+
+        calculator.changeTwoNumbers(num1, num2);
+        System.out.println(num1);
+        System.out.println(num2);
+    }
+}
