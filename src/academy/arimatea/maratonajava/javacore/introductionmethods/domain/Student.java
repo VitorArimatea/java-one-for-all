771165f3e65d333b781a4647bbd4bca6096aa7ff
VitorArimatea <vitorarimatea@hotmail.com>
2025-02-12 08:32:59 -0300

    new: Created Student Class

---
+++ src/academy/arimatea/maratonajava/javacore/introductionmethods/domain/Student.java
@@ -0,0 +1,7 @@
+package academy.arimatea.maratonajava.javacore.introductionmethods.domain;
+
+public class Student {
+    public String name = "Student Default";
+    public int age;
+    public char sex;
+}
