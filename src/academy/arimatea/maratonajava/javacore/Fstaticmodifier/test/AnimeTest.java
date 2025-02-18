1ef184df081cdcaafb5eaca34c0bdaad3b38d39c
VitorArimatea <vitorarimatea@hotmail.com>
2025-02-18 09:33:01 -0300

    New: Static Initialization Example

---
+++ src/academy/arimatea/maratonajava/javacore/Fstaticmodifier/test/AnimeTest.java
@@ -0,0 +1,11 @@
+package academy.arimatea.maratonajava.javacore.Fstaticmodifier.test;
+
+import academy.arimatea.maratonajava.javacore.Fstaticmodifier.domain.Anime;
+
+public class AnimeTest {
+    public static void main(String[] args) {
+        Anime anime = new Anime();
+        Anime anime2 = new Anime();
+        Anime anime3 = new Anime();
+    }
+}
