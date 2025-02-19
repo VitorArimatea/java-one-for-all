1862c1b689be3ee5e0e94abaa9940d6ad7492c0c
VitorArimatea <vitorarimatea@hotmail.com>
2025-02-19 11:06:58 -0300

    New: Override method with super

--- src/academy/arimatea/maratonajava/javacore/Hinheritance/domain/Employee.java
+++ src/academy/arimatea/maratonajava/javacore/Hinheritance/domain/Employee.java
@@ -3,6 +3,13 @@ package academy.arimatea.maratonajava.javacore.Hinheritance.domain;
 public class Employee extends Person {
     private double salary;
 
+    @Override
+    public void print() {
+        super.print();
+        System.out.println("---- Employee Data " + this.getName() + " ----");
+        System.out.println("Salary: " + "R$" + this.salary);
+    }
+
     public double getSalary() {
         return salary;
     }
