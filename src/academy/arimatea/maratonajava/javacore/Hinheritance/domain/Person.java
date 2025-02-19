433ba7ccf02e5ebc528ea7a8bc83c1af616f0d9e
VitorArimatea <vitorarimatea@hotmail.com>
2025-02-19 10:45:16 -0300

    New: Inheritance Example

---
+++ src/academy/arimatea/maratonajava/javacore/Hinheritance/domain/Person.java
@@ -0,0 +1,42 @@
+package academy.arimatea.maratonajava.javacore.Hinheritance.domain;
+
+public class Person {
+    private String name;
+    private String cpf;
+    private Address address;
+
+    public void print() {
+        System.out.println("---- Person Data ----");
+
+        System.out.println("Name: " + name);
+        System.out.println("CPF: " + cpf);
+        System.out.println("Street: " + address.getStreet());
+        System.out.println("City: " + address.getCity());
+        System.out.println("State: " + address.getState());
+        System.out.println("Country: " + address.getCountry());
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+}
