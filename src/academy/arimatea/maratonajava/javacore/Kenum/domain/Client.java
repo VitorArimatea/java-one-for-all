af258093ec6c42cd922fe353388d05419e1e7c40
VitorArimatea <vitorarimatea@hotmail.com>
2025-02-22 19:44:23 -0300

    Update: Added paymentType attribute and cleaned the code

--- src/academy/arimatea/maratonajava/javacore/Kenum/domain/Client.java
+++ src/academy/arimatea/maratonajava/javacore/Kenum/domain/Client.java
@@ -3,33 +3,21 @@ package academy.arimatea.maratonajava.javacore.Kenum.domain;
 public class Client {
     private String name;
     private ClientType clientType;
+    private PaymentType PaymentType;
 
-    public Client(String name, ClientType clientType) {
+    public Client(String name, ClientType clientType, PaymentType paymentType) {
         this.name = name;
         this.clientType = clientType;
+        PaymentType = paymentType;
     }
 
     @Override
     public String toString() {
         return "Client{" +
                 "name='" + name + '\'' +
-                ", clientType=" + clientType +
+                ", clientType=" + clientType.getNameReport() +
+                ", clientType=" + clientType.getValue() +
+                ", PaymentType=" + PaymentType +
                 '}';
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public ClientType getClientType() {
-        return clientType;
-    }
-
-    public void setClientType(ClientType clientType) {
-        this.clientType = clientType;
     }
 }
