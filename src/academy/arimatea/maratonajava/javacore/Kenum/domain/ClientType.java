30300f368d5e810cd41d78fe444588d2351ecb59
VitorArimatea <vitorarimatea@hotmail.com>
2025-02-22 19:50:14 -0300

    Update: translated final's name to English, created value and nameReport attributes

--- src/academy/arimatea/maratonajava/javacore/Kenum/domain/ClientType.java
+++ src/academy/arimatea/maratonajava/javacore/Kenum/domain/ClientType.java
@@ -1,6 +1,22 @@
 package academy.arimatea.maratonajava.javacore.Kenum.domain;
 
 public enum ClientType {
-    PESSOA_FISICA,
-    PESSOA_JURIDICA
+    NATURAL_PERSON(1, "Pessoa Física"),
+    LEGAL_ENTITY(2, "Pessoa Jurídica");
+
+    private int value;
+    private String nameReport;
+
+    ClientType(int value, String nameReport) {
+        this.value = value;
+        this.nameReport = nameReport;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public String getNameReport() {
+        return nameReport;
+    }
 }
