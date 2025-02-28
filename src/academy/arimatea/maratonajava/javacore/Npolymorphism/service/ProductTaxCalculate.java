57695e0476889ca819661df5ecfed5c2abcbbcfe
VitorArimatea <vitorarimatea@hotmail.com>
2025-02-27 21:19:12 -0300

    Update: Added condition for expirationDate if product is an instance of Tomato

--- src/academy/arimatea/maratonajava/javacore/Npolymorphism/service/ProductTaxCalculate.java
+++ src/academy/arimatea/maratonajava/javacore/Npolymorphism/service/ProductTaxCalculate.java
@@ -1,24 +1,25 @@
 package academy.arimatea.maratonajava.javacore.Npolymorphism.service;
 
-import academy.arimatea.maratonajava.javacore.Npolymorphism.domain.Computer;
+import academy.arimatea.maratonajava.javacore.Npolymorphism.domain.Product;
 import academy.arimatea.maratonajava.javacore.Npolymorphism.domain.Tomato;
 
 public class ProductTaxCalculate {
-    public static void taxComputer(Computer computer) {
-        double tax = computer.taxCalculate();
+    public static void calculateTax(Product product) {
+        double tax = product.taxCalculate();
 
         System.out.println("\n----------- Product Tax ----------");
-        System.out.println("Computer: " + computer.getName());
-        System.out.println("Value: " + computer.getValue());
+        System.out.println("Product: " + product.getName());
+        System.out.println("Value: " + product.getValue());
         System.out.println("Tax: " + tax);
-    }
 
-    public static void taxTomato(Tomato tomato) {
-        double tax = tomato.taxCalculate();
+        if(product instanceof Tomato) {
 
-        System.out.println("\n----------- Product Tax ----------");
-        System.out.println("Tomato: " + tomato.getName());
-        System.out.println("Value: " + tomato.getValue());
-        System.out.println("Tax: " + tax);
+//            Other declarations options
+//            ((Tomato) product).getExpirationDate();
+//            String expirationDate = ((Tomato) product).getExpirationDate()
+//            System.out.println((Tomato) product).getExpirationDate();
+
+            System.out.println(tomato.getExpirationDate());
+        }
     }
 }
