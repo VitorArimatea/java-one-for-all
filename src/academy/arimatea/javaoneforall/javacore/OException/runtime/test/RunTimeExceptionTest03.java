a8e14551d5dcfa5dfc44ad8997122ac512b2236a
VitorArimatea <vitorarimatea@hotmail.com>
2025-03-03 12:05:07 -0300

    New: Finally example

---
+++ src/academy/arimatea/javaoneforall/javacore/OException/runtime/test/RunTimeExceptionTest03.java
@@ -0,0 +1,21 @@
+package academy.arimatea.javaoneforall.javacore.OException.runtime.test;
+
+public class RunTimeExceptionTest03 {
+    public static void main(String[] args) {
+        openConnection();
+    }
+
+    private static String openConnection() {
+        try {
+            System.out.println("Opening file...");
+            System.out.println("Writing to file...");
+            System.out.println("Closing resource freed by OS");
+            return "Connection Open";
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("Closing resource freed by the OS");
+        }
+        return null;
+    }
+}
